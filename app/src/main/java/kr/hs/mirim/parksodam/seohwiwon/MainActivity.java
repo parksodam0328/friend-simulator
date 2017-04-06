6e922e16b94148febc4ca61b1de639507f1d798f
Sodam Park <parksodam0228@e-mirim.hs.kr>
2017-04-06 20:16:20 +0900

    #8 첫번째 버튼 동작하게 하기 - onClick()

--- app/src/main/java/kr/hs/mirim/parksodam/seohwiwon/MainActivity.java
+++ app/src/main/java/kr/hs/mirim/parksodam/seohwiwon/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.mirim.parksodam.seohwiwon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void onClick(View view){
+        Toast.makeText(this, "작동 해요!", Toast.LENGTH_SHORT).show(); //.show()를 해야 Toast가 실행된다.
     }
 }
